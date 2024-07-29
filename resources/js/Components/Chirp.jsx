5080bab027ffa2a2aa9a47d74048baa053f9ee8c
zjackson3163 <93844448+zjackson3163@users.noreply.github.com>
2024-07-29 13:34:44 -0400

    delete + chirp log/notification functionality

--- resources/js/Components/Chirp.jsx
+++ resources/js/Components/Chirp.jsx
@@ -45,6 +45,9 @@ export default function Chirp({ chirp }) {
                                 <button className="block w-full px-4 py-2 text-left text-sm leading-5 text-gray-700 hover:bg-gray-100 focus:bg-gray-100 transition duration-150 ease-in-out" onClick={() => setEditing(true)}>
                                     Edit
                                 </button>
+                                <Dropdown.Link as="button" href={route('chirps.destroy', chirp.id)} method="delete">
+                                    Delete
+                                </Dropdown.Link>
                             </Dropdown.Content>
                         </Dropdown>
                     }
